485f612d9f376c39f6cd6401379cd6559d3ef2cf
Sergio Ruiz <SergioMac@MacBook-Air-de-Sergio.local>
2014-06-05 21:41:30 +0200

    Añadido el bucle que itera desde el archivo shows.txt

--- move.sh
+++ move.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Give permissions to the episodes
 
 downloadPath=/media/fd5b5a96-f03b-46b9-94be-a04ce971d3a7/torrent/finish/
 chmod 777 $downloadPath/*
@@ -8,3 +9,11 @@ echo "Changed permissions to the episodes for moving them later";
 echo "--------------------------------------"
 echo "--------------------------------------"
 
+#Move the episodes from the downloads directory to the appropiate folder:
+
+tvShowsList=$(cat shows.txt)
+
+for show in $tvShowsList; do
+        echo "Searching episodes for $show"
+        
+done
